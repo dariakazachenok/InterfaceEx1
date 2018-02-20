457b1ee033002387c422ad05cd0ce1ea3408aa7c
daria kazachenok <kazachenokd@mail.ru>
2018-02-20 16:04:54 +0300

    fix task

--- InterfaceEx1/Program.cs
+++ InterfaceEx1/Program.cs
@@ -1,73 +1,102 @@
 ﻿using System;
 
+
 namespace InterfaceEx1
 {
     interface ISwitchable // объявление интерфейса 1
     {
         void Switchon();
         void Switchoff();
+        void OnScreen();
     }
+
     interface ISwitchAll // объявление интерфейса 2
     {
         void FlatSwitchOffAll();
     }
 
     class TV : ISwitchable //реализация интерфейса 1
+    
     {
+        public int Value;
+        public TV(bool b)
+        {
+            Value = b ? 1 : 0;
+        }
         public void Switchon()
         {
+            Value = 1;
             Console.WriteLine("TV on");
         }
 
         public void Switchoff()
         {
+            Value = 0;
             Console.WriteLine("TV off");
         }
+        public void OnScreen()
+        {
+            Console.WriteLine(Value == 1 ? "TV on" : "TV off");
+        }
     }
-    class Computer : ISwitchable //реализация интерфейса 2
+    class Computer : ISwitchable //реализация интерфейса 1
     {
+        public int Value;
+        public Computer(bool b)
+        {
+            Value = b ? 1 : 0;
+        }
         public void Switchon()
         {
+            Value = 1;
             Console.WriteLine("Computer on");
         }
 
         public void Switchoff()
         {
+            Value = 0;
             Console.WriteLine("Computer off");
+        }
+        public void OnScreen()
+        {
+            Console.WriteLine(Value == 1 ? "Computer on" : "Computer off");
         }
     }
 
     class Lamp : ISwitchable //реализация интерфейса 1
     {
+        public int Value;
+        public Lamp(bool b)
+        {
+            Value = b ? 1 : 0;
+        }
         public void Switchon()
         {
+            Value = 1;
             Console.WriteLine("Lamp on");
         }
 
         public void Switchoff()
         {
+            Value = 0;
             Console.WriteLine("Lamp off");
         }
-    }
-    class SwitchoffAll : ISwitchAll //реализация интерфейса 2
-    {
-        public void FlatSwitchOffAll()
+        public void OnScreen()
         {
-            Console.WriteLine("All off");
+            Console.WriteLine(Value == 1 ? "Lamp on" : "Lamp off");
         }
     }
-
+    
     class InterfaceEx1
     {
         static void Main()
         {
-            TV tv1 = new TV();
-            TV tv2 = new TV();
-            Computer com = new Computer();
-            Lamp lp = new Lamp();
-            SwitchoffAll flat = new SwitchoffAll();
-
-            Console.WriteLine("1- включить 1 телевизор" + "\r\n" + "2- выключить 1 телевизор" + "\r\n" + "3- включить 2 телевизор" + "\r\n" + "4- выключить 2 телевизор" + "\r\n" + "5- включить компьютер" + "\r\n" + "6- выключить компьютер" + "\r\n" + "7- включить лампу" + "\r\n" + "8- выключить лампу" + "\r\n" + "9- All off in flat" + "\r\n");
+            TV tv1 = new TV(true);
+            TV tv2 = new TV(true);
+            Computer com = new Computer(true);
+            Lamp lp = new Lamp(true);
+            
+            Console.WriteLine("1- Turn on 1 TV" + "\r\n" + "2- Turn off 1 TV" + "\r\n" + "3- Turn on 2 TV" + "\r\n" + "4- Turn off 2 TV" + "\r\n" + "5- Turn on computer" + "\r\n" + "Turn off computer" + "\r\n" + "7- Turn on lamp" + "\r\n" + "8- Turn off lamp" + "\r\n" + "9- All off in flat" + "\r\n" + "\r\n" + "10-Devices on? Status" + "\r\n");
             while (true)
             {
                 switch (Convert.ToInt32(Console.ReadLine()))
@@ -79,7 +108,7 @@ namespace InterfaceEx1
                     }
                     case 2:
                     {
-                        tv2.Switchoff();
+                        tv1.Switchoff();
                         break;
                     }
                     case 3:
@@ -115,9 +144,21 @@ namespace InterfaceEx1
                     }
                     case 9:
                     {
-                        flat.FlatSwitchOffAll();
+                        tv1.Switchoff();
+                        tv2.Switchoff();
+                        lp.Switchoff();
+                        com.Switchoff();
                         break;
                     }
+                    case 10:
+                    {
+                        tv1.OnScreen();
+                        tv2.OnScreen();
+                        lp.OnScreen();
+                        com.OnScreen();
+                        break;
+                    }
+
                 }
             }
         }
