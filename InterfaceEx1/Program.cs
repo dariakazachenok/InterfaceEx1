da7d07ef41012b2cd9747bf0eb6ccaa94f6e4581
daria kazachenok <kazachenokd@mail.ru>
2018-02-22 14:32:55 +0300

    changed list devises

--- InterfaceEx1/Program.cs
+++ InterfaceEx1/Program.cs
@@ -7,73 +7,49 @@ namespace InterfaceEx1
     {
         static void Main()
         {
-            List<TV> tv = new List<TV> {new TV(true)};
-            var tv1 = tv[0];
-           
-            /* TV tv1 = new TV(true);
-             TV tv2 = new TV(true); */
+            var tv1 = new TV(true);
+            Computer com1 = new Computer(true);
+            Lamp lp1 = new Lamp(true);
 
-            List<Computer> com = new List<Computer> {new Computer(true)};
-            var com1 = com[0];
-
-            List<Lamp> lp = new List<Lamp> {new Lamp(true)};
-            var lp1 = lp[0];
-
+            List<ISwitchable> devices = new List<ISwitchable> {tv1, com1, lp1};
+            tv1 = (TV) devices[0];
+            com1 = (Computer)devices[1];
+            lp1 = (Lamp)devices[2];
+            
             Console.WriteLine("1- Turn on 1 TV" + "\r\n" + "2- Turn off 1 TV" + "\r\n" + "3- Turn on computer" + "\r\n" + "4-Turn off computer" + "\r\n" + "5- Turn on lamp" + "\r\n" + "6- Turn off lamp" + "\r\n" + "7- All off in flat" + "\r\n" + "\r\n" + "8-Devices on? Status" + "\r\n");
             while (true)
             {
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
-                    {
                         tv1.Switchon();
                         break;
-                    }
                     case 2:
-                    {
                         tv1.Switchoff();
                         break;
-                    }
                     case 3:
-                    {
                         com1.Switchon();
                         break;
-                    }
                     case 4:
-                    {
                         com1.Switchoff();
                         break;
-                    }
                     case 5:
-                    {
                         lp1.Switchon();
                         break;
-                    }
                     case 6:
-                    {
                         lp1.Switchoff();
                         break;
-                    }
                     case 7:
-                    {
-                       foreach (var t in tv)
-                       t.Switchoff();
-
-                      foreach (Computer c in com)
-                       c.Switchoff();
-
-                       foreach (Lamp l in lp)
-                        l.Switchoff();
-                       break;
-                    }
+                        foreach (var d in devices)
+                        {
+                            d.Switchoff();
+                        }
+                        break;
                     case 8:
-                    {
                         tv1.OnScreen();
                         lp1.OnScreen();
                         com1.OnScreen();
-                       break;
-                    }
-
+                        break;
                 }
             }
         }
